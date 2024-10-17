e2cc9975b4bbfc9b6def5df0ca759bbcddb7b1ea
MaiberCordoba <cordobamaiber@gmail.com>
2024-10-17 16:29:46 -0500

    arreglando error en el modelo productos, agregando parametros de la cantidad max ed caracteres digitados

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Productos(models.Model):
     nombre=models.CharField(max_length=30)
-    descripcion=models.TextField
-    precio=models.DecimalField
-    
+    descripcion=models.TextField(max_length=255)
+    precio=models.DecimalField(max_digits=10, decimal_places=2)
+  
     def __str__(self):
         return self.nombre
